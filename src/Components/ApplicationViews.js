9f5aa73232b9f5c956e989815693c03e2e610cd3
scottsilver <scottsilver47@gmail.com>
2019-07-14 14:54:55 -0500

    added owners array in my state and a link in navigation bar that links to the /owners path

--- src/Components/ApplicationViews.js
+++ src/Components/ApplicationViews.js
@@ -26,10 +26,19 @@ class ApplicationViews extends Component {
         { id: 6, name: "Checkers" }
     ]
 
+    ownersFromAPI = [
+        { id: 1, phone: "615-999-9999", name: "Doodle Dan"},
+        { id: 2, phone: "615-999-9998", name: "Jovial Jack"},
+        { id: 3, phone: "615-999-9997", name: "Anxious Angus"},
+        { id: 4, phone: "615-999-9996", name: "Hip Henley"},
+        { id: 5, phone: "615-999-9995", name: "Daring Derkins"},
+        { id: 6, phone: "615-999-9999", name: "Chubby Checkers"}
+    ]
     state = {
         employees: this.employeesFromAPI,
         locations: this.locationsFromAPI,
-        animals: this.animalsFromAPI
+        animals: this.animalsFromAPI,
+        owners: this.ownersFromAPI
     }
 
     render() {
@@ -43,6 +52,9 @@ class ApplicationViews extends Component {
                 }} />
                 <Route path="/employees" render={(props) => {
                     return <EmployeeList employees={this.state.employees} />
+                }} />
+                <Route path="/owners" render={(props) => {
+                    return <EmployeeList owners={this.state.owners} />
                 }} />
             </React.Fragment>
         )
