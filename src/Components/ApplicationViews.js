3b869fc005b2dc08f860304bd6f6001a0063a6f0
scottsilver <scottsilver47@gmail.com>
2019-07-15 13:54:50 -0500

    created manager file for animals, locations, employees, & owners & added getAll methods to each one and refactored Application views to import all of them

--- src/Components/ApplicationViews.js
+++ src/Components/ApplicationViews.js
@@ -4,7 +4,12 @@ import AnimalList from './animal/AnimalList'
 import LocationList from './location/LocationList'
 import EmployeeList from './employee/EmployeeList'
 import OwnerList from './owner/ownerList'
+import AnimalManager from "../modules/AnimalManager"
+import OwnerManager from '../modules/OwnerManager';
+import EmployeeManager from '../modules/EmployeeManager';
+import LocationManager from '../modules/EmployeeManager';
 
+// import AnimalManager from "../modules/AnimalManager"
 export default class ApplicationViews extends Component {
 
     state = {
@@ -14,33 +19,43 @@ export default class ApplicationViews extends Component {
         owners: []
     }
 
-componentDidMount() {
-    const newState = {}
+    componentDidMount() {
+        const newState = {}
 
-    fetch("http://localhost:5002/animals")
-        .then(r => r.json())
-        .then(animals => newState.animals = animals)
-        .then(() => fetch("http://localhost:5002/employees")
-        .then(r => r.json()))
-        .then(employees => newState.employees = employees)
-        .then(() => fetch("http://localhost:5002/owners")
-        .then(r => r.json()))
-        .then(owners => newState.owners = owners)
-        .then(() => this.setState(newState))
-}
+        AnimalManager.getAll()
+            .then(animals => newState.animals = animals)
+        EmployeeManager.getAll()
+            .then(employees => newState.employees = employees)
+        OwnerManager.getAll()
+            .then(owners => newState.owners = owners)
+        LocationManager.getAll()
+            .then(locations => newState.locations = locations)
+            .then(() => this.setState(newState))
+    }
+    deleteAnimal = id => {
+        return fetch(`http://localhost:5002/animals/${id}`, {
+            method: "DELETE"
+        })
+        .then(e => e.json())
+        .then(() => fetch(`http://localhost:5002/animals`))
+        .then(e => e.json())
+        .then(animals => this.setState({
+            animals: animals
+        })
+        )
+    }
 
-deleteAnimal = id => {
-    return fetch(`http://localhost:5002/animals/${id}`, {
-        method: "DELETE"
-    })
-    .then(e => e.json())
-    .then(() => fetch(`http://localhost:5002/animals`))
-    .then(e => e.json())
-    .then(animals => this.setState({
-        animals: animals
-    })
-  )
-}
+    deleteOwner = id => {
+        return fetch(`http://localhost:5002/owners/${id}`, {
+            method: "DELETE"
+        })
+        .then(e => e.json())
+        .then(() => fetch(`http://localhost:5002/owners`))
+        .then(e => e.json())
+        .then(owners => this.setState({
+            owners: owners
+        }))
+    }
 
     render() {
         return (
@@ -55,11 +70,10 @@ deleteAnimal = id => {
                     return <EmployeeList employees={this.state.employees} />
                 }} />
                 <Route path="/owners" render={(props) => {
-                    return <OwnerList owners={this.state.owners} />
+                    return <OwnerList deleteOwner={this.deleteOwner} owners={this.state.owners} />
                 }} />
             </React.Fragment>
         )
     }
 
 }
-
