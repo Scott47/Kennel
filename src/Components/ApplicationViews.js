39730d83e11814ad6668bce1124313b221e00a23
scottsilver <scottsilver47@gmail.com>
2019-07-15 11:29:08 -0500

    added delete functionality for animals and employees with card styling

--- src/Components/ApplicationViews.js
+++ src/Components/ApplicationViews.js
@@ -5,42 +5,38 @@ import LocationList from './location/LocationList'
 import EmployeeList from './employee/EmployeeList'
 import OwnerList from './owner/ownerList'
 
-class ApplicationViews extends Component {
-    employeesFromAPI = [
-        { id: 1, name: "Jessica Younker" },
-        { id: 2, name: "Jordan Nelson" },
-        { id: 3, name: "Zoe LeBlanc" },
-        { id: 4, name: "Blaise Roberts" }
-    ]
+export default class ApplicationViews extends Component {
 
-    locationsFromAPI = [
-        { id: 1, name: "Nashville North", address: "500 Circle Way" },
-        { id: 2, name: "Nashville South", address: "10101 Binary Court" }
-    ]
-
-    animalsFromAPI = [
-        { id: 1, name: "Doodles" },
-        { id: 2, name: "Jack" },
-        { id: 3, name: "Angus" },
-        { id: 4, name: "Henley" },
-        { id: 5, name: "Derkins" },
-        { id: 6, name: "Checkers" }
-    ]
-
-    ownersFromAPI = [
-        { id: 1, phone: "615-999-9999", name: "Doodle Dan"},
-        { id: 2, phone: "615-999-9998", name: "Jovial Jack"},
-        { id: 3, phone: "615-999-9997", name: "Anxious Angus"},
-        { id: 4, phone: "615-999-9996", name: "Hip Henley"},
-        { id: 5, phone: "615-999-9995", name: "Daring Derkins"},
-        { id: 6, phone: "615-999-9999", name: "Chubby Checkers"}
-    ]
     state = {
-        employees: this.employeesFromAPI,
-        locations: this.locationsFromAPI,
-        animals: this.animalsFromAPI,
-        owners: this.ownersFromAPI
+        locations: [],
+        animals: [],
+        employees: []
     }
+
+componentDidMount() {
+    const newState = {}
+
+    fetch("http://localhost:5002/animals")
+        .then(r => r.json())
+        .then(animals => newState.animals = animals)
+        .then(() => fetch("http://localhost:5002/employees")
+        .then(r => r.json()))
+        .then(employees => newState.employees = employees)
+        .then(() => this.setState(newState))
+}
+
+deleteAnimal = id => {
+    return fetch(`http://localhost:5002/animals/${id}`, {
+        method: "DELETE"
+    })
+    .then(e => e.json())
+    .then(() => fetch(`http://localhost:5002/animals`))
+    .then(e => e.json())
+    .then(animals => this.setState({
+        animals: animals
+    })
+  )
+}
 
     render() {
         return (
@@ -49,7 +45,7 @@ class ApplicationViews extends Component {
                     return <LocationList locations={this.state.locations} />
                 }} />
                 <Route path="/animals" render={(props) => {
-                    return <AnimalList animals={this.state.animals} />
+                    return <AnimalList deleteAnimal={this.deleteAnimal} animals={this.state.animals} />
                 }} />
                 <Route path="/employees" render={(props) => {
                     return <EmployeeList employees={this.state.employees} />
@@ -60,6 +56,6 @@ class ApplicationViews extends Component {
             </React.Fragment>
         )
     }
+
 }
 
-export default ApplicationViews
